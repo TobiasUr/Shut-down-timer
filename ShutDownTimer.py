d8b39b5b8574fbeeca5f3197b3ca54068a747ce2
TobiasUr <68461330+TobiasUr@users.noreply.github.com>
2022-09-17 15:12:50 +0200

    Update ShutDownTimer.py
    Now you can know how many time you have left

--- ShutDownTimer.py
+++ ShutDownTimer.py
@@ -1,11 +1,54 @@
+from cgitb import text
 import os
+from socket import socket
 from tkinter import *
+import time as tm
+import datetime
+import threading
+
 root = Tk()
+
 Label(root, text="Enter time to shut down in minutes: ").grid(row=0, column=0)
+Label(root, text="Time left until shutdown: ").grid(row=1, column=0)
+
+global v
+v = StringVar()
+Label(root, textvariable=v).grid(row=1, column=1)
+
 e=Entry(root, width=10, borderwidth=5)
 e.grid(row=0, column=1)
-time=e.get()
+
+global time 
+time = 0
+
+global secondsleft
+
+def countdown():
+    global time
+    global secondsleft
+    secondsleft = time
+    seconds = time
+    while seconds > 0:
+        root.update
+        timer = datetime.timedelta(seconds= seconds)
+        tm.sleep(1)
+        seconds -= 1
+        secondsleft -= 1
+        global v
+        if secondsleft <1:
+            v.set("")
+            break
+        v.set(str(seconds))
+        
+        
+
+
+        
+
 def OK():
+    cancel()
+    tm.sleep(1)
+    global time
     time=int(e.get())
     time=time * 60
     stringOne="shutdown /s /t "
@@ -13,9 +56,12 @@ def OK():
     StringFinal = stringOne + StringTwo
     print(StringFinal)
     os.system(StringFinal)
+    threading.Thread(target=countdown).start()
 
 def cancel():
     os.system('shutdown -a')
+    global secondsleft
+    secondsleft=0
 
 Button(root, text="OK", command=OK).grid(row=0, column=2)
 Button(root, text="Cancel", command=cancel).grid(row=0, column=3)
